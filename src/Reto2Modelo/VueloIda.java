c3fe1550570cf9ed9d9b5c70632c56e6a3ad6d6b
daniel-alonsome <daniel.alonsome@elorrieta-errekamari.com>
2025-01-23 10:47:01 +0100

    Clase pojos Vuelo Ida añadido atributo Aerolinea

--- src/Reto2Modelo/VueloIda.java
+++ src/Reto2Modelo/VueloIda.java
@@ -8,19 +8,21 @@ public class VueloIda extends Evento {
 	private String DuracionVuelo;
 	private Aeropuerto AeropuertoOrigen;
 	private Aeropuerto AeropuertoDestino;
+	private Aerolinea Aerolinea;
 
-
-	public VueloIda(String eventoID, String nombreEvento, String precio, Reto2Modelo.Viaje viaje, String codigoVuelo,
+	public VueloIda(String eventoID, String nombreEvento, String precio, Viaje viajeID, String codigoVuelo,
 			String fechaSalida, String horaSalida, String duracionVuelo, Aeropuerto aeropuertoOrigen,
-			Aeropuerto aeropuertoDestino) {
-		super(eventoID, nombreEvento, precio, viaje);
+			Aeropuerto aeropuertoDestino, Reto2Modelo.Aerolinea aerolinea) {
+		super(eventoID, nombreEvento, precio, viajeID);
 		CodigoVuelo = codigoVuelo;
 		FechaSalida = fechaSalida;
 		HoraSalida = horaSalida;
 		DuracionVuelo = duracionVuelo;
 		AeropuertoOrigen = aeropuertoOrigen;
 		AeropuertoDestino = aeropuertoDestino;
+		Aerolinea = aerolinea;
 	}
+	
 	public Aeropuerto getAeropuertoOrigen() {
 		return AeropuertoOrigen;
 	}
@@ -57,14 +59,20 @@ public class VueloIda extends Evento {
 	public void setDuracionVuelo(String duracionVuelo) {
 		DuracionVuelo = duracionVuelo;
 	}
-	
 	@Override
 	public String toString() {
-		return super.toString()+"VueloIda [CodigoVuelo=" + CodigoVuelo + ", FechaSalida=" + FechaSalida + ", HoraSalida=" + HoraSalida
+		return  super.toString()+"VueloIda [CodigoVuelo=" + CodigoVuelo + ", FechaSalida=" + FechaSalida + ", HoraSalida=" + HoraSalida
 				+ ", DuracionVuelo=" + DuracionVuelo + ", AeropuertoOrigen=" + AeropuertoOrigen + ", AeropuertoDestino="
-				+ AeropuertoDestino + "]";
+				+ AeropuertoDestino + ", Aerolinea=" + Aerolinea + "]";
+	}
+	public Aerolinea getAerolinea() {
+		return Aerolinea;
+	}
+	public void setAerolinea(Aerolinea aerolinea) {
+		Aerolinea = aerolinea;
 	}
 	
+
 	
 	
 	}
