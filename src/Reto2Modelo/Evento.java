1bb92925197a6389ee4ebb0ed680b56dd0c3d4f9
Iker836 <iker.arrebolato@elorrieta-errekamari.com>
2025-01-23 10:30:29 +0100

    Clase pojos Evento con Constructor con atributo Viaje de la clase Viaje

--- src/Reto2Modelo/Evento.java
+++ src/Reto2Modelo/Evento.java
@@ -5,14 +5,14 @@ public class Evento {
 	private String EventoID;
 	private String NombreEvento;
 	private String Precio;
+	private Viaje Viaje;
 	
-	public Evento() {
-		
-	}
-	public Evento(String eventoID, String nombreEvento, String precio) {
+
+	public Evento(String eventoID, String nombreEvento, String precio,Viaje viaje) {
 		EventoID = eventoID;
 		NombreEvento = nombreEvento;
 		Precio = precio;
+		Viaje = viaje;
 	}
 	public String getEventoID() {
 		return EventoID;
@@ -32,9 +32,16 @@ public class Evento {
 	public void setPrecio(String precio) {
 		Precio = precio;
 	}
+	public Viaje getViaje() {
+		return Viaje;
+	}
+	public void setViaje(Viaje viaje) {
+		Viaje = viaje;
+	}
 	@Override
 	public String toString() {
-		return "Evento [EventoID=" + EventoID + ", NombreEvento=" + NombreEvento + ", Precio=" + Precio + "]";
+		return "Evento [EventoID=" + EventoID + ", NombreEvento=" + NombreEvento + ", Precio=" + Precio + ", Viaje="
+				+ Viaje + "]";
 	}
-	
+
 }
