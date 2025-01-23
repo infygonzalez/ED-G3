9f8fa078ccee8575d402c9bf859d06feaf9d8175
Iker836 <iker.arrebolato@elorrieta-errekamari.com>
2025-01-23 09:40:34 +0100

    Clase pojos de la tabla Aerolinea con getters, setters y toString

---
+++ src/Reto2Modelo/Aerolinea.java
@@ -0,0 +1,25 @@
+package Reto2Modelo;
+
+public class Aerolinea {
+	
+	private String CodigoAerolinea;
+	private String NombreAerolinea;
+	public String getCodigoAerolinea() {
+		return CodigoAerolinea;
+	}
+	public void setCodigoAerolinea(String codigoAerolinea) {
+		CodigoAerolinea = codigoAerolinea;
+	}
+	public String getNombreAerolinea() {
+		return NombreAerolinea;
+	}
+	public void setNombreAerolinea(String nombreAerolinea) {
+		NombreAerolinea = nombreAerolinea;
+	}
+	@Override
+	public String toString() {
+		return "Aerolinea [CodigoAerolinea=" + CodigoAerolinea + ", NombreAerolinea=" + NombreAerolinea + "]";
+	}
+
+
+}
