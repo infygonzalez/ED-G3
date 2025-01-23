09423bc1cdc05d733f9e60daa26f3d2bea3344a9
daniel-alonsome <daniel.alonsome@elorrieta-errekamari.com>
2025-01-23 10:43:58 +0100

    Clase pojos Vuelo Ida añadido atributos Aeropuerto Origen y Destino

--- src/Reto2Modelo/VueloIda.java
+++ src/Reto2Modelo/VueloIda.java
@@ -6,14 +6,32 @@ public class VueloIda extends Evento {
 	private String FechaSalida;
 	private String HoraSalida;
 	private String DuracionVuelo;
-	
-	
-	public VueloIda(String eventoID, String nombreEvento, String precio,Viaje viaje,String codigoVuelo, String fechaSalida, String horaSalida, String duracionVuelo) {
-		super(eventoID,nombreEvento,precio, viaje );
+	private Aeropuerto AeropuertoOrigen;
+	private Aeropuerto AeropuertoDestino;
+
+
+	public VueloIda(String eventoID, String nombreEvento, String precio, Reto2Modelo.Viaje viaje, String codigoVuelo,
+			String fechaSalida, String horaSalida, String duracionVuelo, Aeropuerto aeropuertoOrigen,
+			Aeropuerto aeropuertoDestino) {
+		super(eventoID, nombreEvento, precio, viaje);
 		CodigoVuelo = codigoVuelo;
 		FechaSalida = fechaSalida;
 		HoraSalida = horaSalida;
 		DuracionVuelo = duracionVuelo;
+		AeropuertoOrigen = aeropuertoOrigen;
+		AeropuertoDestino = aeropuertoDestino;
+	}
+	public Aeropuerto getAeropuertoOrigen() {
+		return AeropuertoOrigen;
+	}
+	public void setAeropuertoOrigen(Aeropuerto aeropuertoOrigen) {
+		AeropuertoOrigen = aeropuertoOrigen;
+	}
+	public Aeropuerto getAeropuertoDestino() {
+		return AeropuertoDestino;
+	}
+	public void setAeropuertoDestino(Aeropuerto aeropuertoDestino) {
+		AeropuertoDestino = aeropuertoDestino;
 	}
 	public String getCodigoVuelo() {
 		return CodigoVuelo;
@@ -39,11 +57,14 @@ public class VueloIda extends Evento {
 	public void setDuracionVuelo(String duracionVuelo) {
 		DuracionVuelo = duracionVuelo;
 	}
+	
 	@Override
 	public String toString() {
-		return  super.toString()+"VueloIda [CodigoVuelo=" + CodigoVuelo + ", FechaSalida=" + FechaSalida + ", HoraSalida=" + HoraSalida
-				+ ", DuracionVuelo=" + DuracionVuelo + "]";
+		return super.toString()+"VueloIda [CodigoVuelo=" + CodigoVuelo + ", FechaSalida=" + FechaSalida + ", HoraSalida=" + HoraSalida
+				+ ", DuracionVuelo=" + DuracionVuelo + ", AeropuertoOrigen=" + AeropuertoOrigen + ", AeropuertoDestino="
+				+ AeropuertoDestino + "]";
 	}
+	
 	
 	
 	}
