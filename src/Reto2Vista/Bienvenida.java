e99a373270cc3649d0006e6bea82fb94e947c575
daniel-alonsome <daniel.alonsome@elorrieta-errekamari.com>
2025-01-28 08:50:41 +0100

    Pantalla Bienvenida Añadir foto de fondo

--- src/Reto2Vista/Bienvenida.java
+++ src/Reto2Vista/Bienvenida.java
@@ -1,10 +1,15 @@
 package Reto2Vista;
 
 import java.awt.EventQueue;
+import java.io.InputStream;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import java.awt.Image;
 
 public class Bienvenida extends JFrame {
 
@@ -17,12 +22,25 @@ public class Bienvenida extends JFrame {
 	 * Create the frame.
 	 */
 	public Bienvenida() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 500);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 
 		setContentPane(contentPane);
-	}
 
+
+		ImageIcon image = new ImageIcon(getClass().getResource("/Reto2Img/fondo1.jpg"));
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(0, 0, 0, 0));
+		contentPane.add(panel);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER,0, 0));
+		
+		JLabel lblNewLabel = new JLabel(new ImageIcon(image.getImage().getScaledInstance(800, 500, Image.SCALE_DEFAULT)));
+		panel.add(lblNewLabel);
+		
+	}
 }
