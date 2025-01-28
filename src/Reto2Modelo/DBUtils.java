c7511d920c11f716577058c1ae2ed9840e00f5c5
daniel-alonsome <daniel.alonsome@elorrieta-errekamari.com>
2025-01-28 09:12:38 +0100

    DBUtils: conectar con nuestra base de datos

--- src/Reto2Modelo/DBUtils.java
+++ src/Reto2Modelo/DBUtils.java
@@ -2,4 +2,8 @@ package Reto2Modelo;
 
 public class DBUtils {
 
+	public static final String URL = "jdbc:mysql://localhost:33060/viajeserrekamari";
+	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	public static final String USER = "root";
+	public static final String PASSWORD = "elorrieta";
 }
