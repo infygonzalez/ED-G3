28a443e1b83ebbf8448b1e1e3f3478395bb711ad
daniel-alonsome <daniel.alonsome@elorrieta-errekamari.com>
2025-01-28 09:14:02 +0100

    SQLQueries: Select agencia login

--- src/Reto2Modelo/SQLQueries.java
+++ src/Reto2Modelo/SQLQueries.java
@@ -1,5 +1,6 @@
 package Reto2Modelo;
 
 public class SQLQueries {
-
+	// SELECTS
+		public static final String SELECT_AGENCIA_NOMBRE_CONTRASEÑA = "SELECT * FROM agencia WHERE NombreAgencia = ? and Contraseña = ?";
 }
