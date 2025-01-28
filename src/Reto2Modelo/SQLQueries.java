77c7bd0db1fac811d4baf6326388c50063ae88c4
daniel-alonsome <daniel.alonsome@elorrieta-errekamari.com>
2025-01-28 10:12:38 +0100

    Añadir sql a module.info

--- src/Reto2Modelo/SQLQueries.java
+++ src/Reto2Modelo/SQLQueries.java
@@ -2,5 +2,5 @@ package Reto2Modelo;
 
 public class SQLQueries {
 	// SELECTS
-		public static final String SELECT_AGENCIA_NOMBRE_CONTRASEÑA = "SELECT * FROM agencia WHERE NombreAgencia = ? and Contraseña = ?";
+	public static final String SELECT_AGENCIA_NOMBRE_CONTRASEÑA = "SELECT * FROM agencia WHERE NombreAgencia = ? and Contraseña = ?";
 }
--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,6 @@
  */
 module EDG3 {
 	requires java.desktop;
+	requires java.sql;
+
 }
