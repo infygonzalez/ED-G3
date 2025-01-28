7937b11fed29f9aa43798f81c7872ae22f3edb79
Iker836 <iker.arrebolato@elorrieta-errekamari.com>
2025-01-28 10:32:08 +0100

    Añadir atributo contraseña en la clase pojos de Agencia

--- src/Reto2Modelo/Agencia.java
+++ src/Reto2Modelo/Agencia.java
@@ -9,20 +9,29 @@ public class Agencia {
 	private String NumeroEmpleados;
 	private String TipoAgencia;
 	private ArrayList<Viaje> Viajes;
+	private String Contraseña;
+
+	
 
 	public Agencia() {
 		
 	}
 
+	
+
 	public Agencia(String nombreAgencia, String logo, String colorMarca, String numeroEmpleados, String tipoAgencia,
-			ArrayList<Viaje> viajes) {
+			ArrayList<Viaje> viajes, String contraseña) {
+		super();
 		NombreAgencia = nombreAgencia;
 		Logo = logo;
 		ColorMarca = colorMarca;
 		NumeroEmpleados = numeroEmpleados;
 		TipoAgencia = tipoAgencia;
 		Viajes = viajes;
+		Contraseña = contraseña;
 	}
+
+
 
 	public ArrayList<Viaje> getViajes() {
 		return Viajes;
@@ -61,12 +70,21 @@ public class Agencia {
 	public void setTipoAgencia(String tipoAgencia) {
 		TipoAgencia = tipoAgencia;
 	}
+	public String getContraseña() {
+		return Contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		Contraseña = contraseña;
+	}
+
+
+
 	@Override
 	public String toString() {
 		return "Agencia [NombreAgencia=" + NombreAgencia + ", Logo=" + Logo + ", ColorMarca=" + ColorMarca
-				+ ", NumeroEmpleados=" + NumeroEmpleados + ", TipoAgencia=" + TipoAgencia + ", Viajes=" + Viajes + "]";
+				+ ", NumeroEmpleados=" + NumeroEmpleados + ", TipoAgencia=" + TipoAgencia + ", Viajes=" + Viajes
+				+ ", Contraseña=" + Contraseña + "]";
 	}
-
-	
 }
 	
