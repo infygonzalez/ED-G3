c0d5e3968b82fa457fb605229de739005c08dfdc
daniel-alonsome <daniel.alonsome@elorrieta-errekamari.com>
2025-01-29 12:56:56 +0100

    SQLQueries: cambiar viajes por viaje

--- src/Reto2Modelo/SQLQueries.java
+++ src/Reto2Modelo/SQLQueries.java
@@ -4,7 +4,7 @@ public class SQLQueries {
 	// SELECTS
 
 	public static final String SELECT_AGENCIA_NOMBRE_CONTRASEÑA = "SELECT * FROM agencia WHERE NombreAgencia = ? and Contraseña = ?";
-	public static final String SELECT_VIAJES_AGENCIA = "SELECT * FROM viajes WHERE NombreAgencia=?";
+	public static final String SELECT_VIAJES_AGENCIA = "SELECT * FROM viaje WHERE NombreAgencia=?";
 	public static final String SELECT_TODOS_PAISES = "SELECT * FROM Pais ";
 	public static final String SELECT_TODOS_AEROPUERTOS = "SELECT * FROM Aeropuerto ";
 	public static final String SELECT_TODOS_AEROLINEAS = "SELECT * FROM Aerolinea ";
