b3da15befe382bc3ca9078073eaaf84c922afc5d
daniel-alonsome <daniel.alonsome@elorrieta-errekamari.com>
2025-01-30 11:57:27 +0100

    SQLQueries: Actualizar select vuelos

--- src/Reto2Modelo/SQLQueries.java
+++ src/Reto2Modelo/SQLQueries.java
@@ -8,7 +8,7 @@ public class SQLQueries {
 	public static final String SELECT_TODOS_PAISES = "SELECT * FROM Pais ";
 	public static final String SELECT_TODOS_AEROPUERTOS = "SELECT * FROM Aeropuerto ";
 	public static final String SELECT_TODOS_AEROLINEAS = "SELECT * FROM Aerolinea ";
-	public static final String SELECT_VUELOS_VIAJE = "SELECT * FROM vuelo WHERE ViajeID=?";
+	public static final String SELECT_VUELOS_VIAJE = "SELECT * FROM Vuelo v WHERE ViajeID = ? AND (v.EventoVueltaID IS NOT NULL  OR v.EventoVueltaID IS NULL AND v.EventoID NOT IN (SELECT  EventoVueltaID FROM Vuelo WHERE EventoVueltaID IS NOT NULL));";
 	public static final String SELECT_ALOJAMIENTO_VIAJE = "SELECT * FROM alojamiento WHERE ViajeID=?";
 	public static final String SELECT_OTROS_VIAJE = "SELECT * FROM otros WHERE ViajeID=?";
 
