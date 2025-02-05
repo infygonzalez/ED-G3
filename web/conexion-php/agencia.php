26e386ca385b4bed9b5d4f97be309ad1ce8ff37c
daniel-alonsome <daniel.alonsome@elorrieta-errekamari.com>
2025-02-05 12:39:28 +0100

    Web: color de texto segun el fondo

--- web/conexion-php/agencia.php
+++ web/conexion-php/agencia.php
@@ -6,24 +6,41 @@ $logoAgencia = "";
 $login = false;
 session_start();
 if (isset($_SESSION['Agencia'])) {
-    $nombreAgencia = $_SESSION['Agencia'];
-    $sql = "SELECT * FROM agencia WHERE NombreAgencia = '$nombreAgencia'";
-    $resultado = $conn->query($sql);
+  $nombreAgencia = $_SESSION['Agencia'];
+  $sql = "SELECT * FROM agencia WHERE NombreAgencia = '$nombreAgencia'";
+  $resultado = $conn->query($sql);
 
-    if ($resultado->num_rows == 1) {
-        $login = true;
-        $row = $resultado->fetch_assoc();
-        $colorAgencia = $row['ColorMarca'];
-        $logoAgencia = $row['Logo'];
-    }
+  if ($resultado->num_rows == 1) {
+    $login = true;
+    $row = $resultado->fetch_assoc();
+    $colorAgencia = $row['ColorMarca'];
+    $logoAgencia = $row['Logo'];
+  }
 }
 if ($login == false) {
-    header(header: "location: /login.php");
+  header(header: "location: /login.php");
+}
+function colorTexto($hex)
+{
+  list($red, $green, $blue) = sscanf($hex, "#%02x%02x%02x");
+  $luma = ($red + $green + $blue) / 3;
+
+  if ($luma < 128) {
+    $textcolour = "white";
+  } else {
+    $textcolour = "black";
+  }
+  return $textcolour;
 }
 ?>
-  <style>
-    * {
-      --coloragencia: <?php echo $colorAgencia; ?>;
-      --coloragenciatexto: white;
-    }
-  </style>
+<style>
+  * {
+    --coloragencia:
+      <?php echo $colorAgencia; ?>
+    ;
+    --coloragenciatexto:
+      <?php echo colorTexto($colorAgencia); ?>
+    ;
+    ;
+  }
+</style>
